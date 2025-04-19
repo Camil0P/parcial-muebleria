42c76203f64f77cc0229defb1b82154bdb70e9c5
Camilo Perez <caperez2244@gmail.com>
2025-04-18 23:29:46 -0500

    conecion base

--- backend/src/controllers/categoria.controllers.js
+++ backend/src/controllers/categoria.controllers.js
@@ -1,6 +1,15 @@
-const getCategorias = (req, res)=>{
-    res.json({"Categoria":"silla"})
+import getConnection from "../db/database.js"
+
+const getCategorias = async (req, res)=>{
+    try {
+        const connection = await getConnection();
+        const result = await connection.query("SELECT id, nombre FROM categorias");
+        res.json(result);   
+    } catch (error) {
+        console.error("ERROR 500")
+    }
 }
+
 
 export const methodHTTP = {
     getCategorias
